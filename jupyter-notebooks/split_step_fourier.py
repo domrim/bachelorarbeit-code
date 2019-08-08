77eae8759f07fd575192340e5c380abb9224b08e
Dominik Rimpf <dominik.rimpf@student.kit.edu>
2019-08-08 14:45:59 +0200

    [FEAT] Changed parameter order of ir-functions. Added time index return

--- jupyter-notebooks/split_step_fourier.py
+++ jupyter-notebooks/split_step_fourier.py
@@ -7,11 +7,8 @@ import numpy as np
 
 # Filter Definitions
 
-# Raised-Cosine
-# find impulse response of an RC filter
 # taken from https://github.com/kit-cel/lecture-examples/blob/master/nt1/vorlesung/3_mod_demod/pulse_shaping.ipynb
-def get_rc_ir(K, n_up, t_symbol, r):
-    
+def get_rc_ir(K, r, f_symbol, n_up):
     ''' 
     Determines coefficients of an RC filter 
     
@@ -20,77 +17,102 @@ def get_rc_ir(K, n_up, t_symbol, r):
     
     NOTE: Length of the IR has to be an odd number
     
-    IN: length of IR (K), upsampling factor (n_up), symbol time (t_symbol), roll-off factor (r)
-    OUT: filter coefficients (rc)
+    IN: length of IR (K), roll-off factor (r), symbol rate (f_symbol), upsampling factor (n_up) 
+    OUT: time values (t_index), filter coefficients (ir)
     '''
 
     # check that IR length is odd
     assert K % 2 == 1, 'Length of the impulse response should be an odd number'
     
     # initialize output length and sample time
-    rc = np.zeros( K )
-    t_sample = t_symbol / n_up
+    ir = np.zeros( K )
+    T_symbol = 1.0 / f_symbol
+    t_sample = T_symbol / n_up
     
     # time indices and sampled time
     k_steps = np.arange( -(K-1) / 2.0, (K-1) / 2.0 + 1 )   
     t_steps = k_steps * t_sample
+    t_index = np.arange( K ) / f_symbol / n_up
     
     for k in k_steps.astype(int):
         
         if t_steps[k] == 0:
-            rc[ k ] = 1. / t_symbol
+            ir[ k ] = 1. / T_symbol
             
-        elif r != 0 and np.abs( t_steps[k] ) == t_symbol / ( 2.0 * r ):
-            rc[ k ] = r / ( 2.0 * t_symbol ) * np.sin( np.pi / ( 2.0 * r ) )
+        elif r != 0 and np.abs( t_steps[k] ) == T_symbol / ( 2.0 * r ):
+            ir[ k ] = r / ( 2.0 * T_symbol ) * np.sin( np.pi / ( 2.0 * r ) )
             
         else:
-            rc[ k ] = np.sin( np.pi * t_steps[k] / t_symbol ) / np.pi / t_steps[k]                 * np.cos( r * np.pi * t_steps[k] / t_symbol )                 / ( 1.0 - ( 2.0 * r * t_steps[k] / t_symbol )**2 )
- 
-    return rc
+            ir[ k ] = np.sin( np.pi * t_steps[k] / T_symbol ) / np.pi / t_steps[k]                 * np.cos( r * np.pi * t_steps[k] / T_symbol )                 / ( 1.0 - ( 2.0 * r * t_steps[k] / T_symbol )**2 )
+     
+    return t_index, ir
 
 
-def get_rrc_ir(K, n_up, t_symbol, r):
+def get_rrc_ir(K, r, f_symbol, n_up):
+    ''' 
+    Determines coefficients of an RRC filter 
+    
+    Formula out of: K.-D. Kammeyer, Nachrichtenübertragung
+    At poles, l'Hospital was used 
+    
+    NOTE: Length of the IR has to be an odd number
+    
+    IN: length of IR (K), roll-off factor (r), symbol rate (f_symbol), upsampling factor (n_up) 
+    OUT: time values (t_index), filter coefficients (ir)
+    '''
     
     # check that IR length is odd
     assert K % 2 == 1, 'Length of the impulse response should be an odd number'
     
     # initialize output length and sample time
-    rrc = np.zeros( K )
-    t_sample = t_symbol / n_up
+    ir = np.zeros( K )
+    T_symbol = 1.0 / f_symbol
+    t_sample = T_symbol / n_up
     
     # time indices and sampled time
     k_steps = np.arange( -(K-1) / 2.0, (K-1) / 2.0 + 1 )   
     t_steps = k_steps * t_sample
+    t_index = np.arange( K ) / f_symbol / n_up
     
     for k in k_steps.astype(int):
 
         if t_steps[k] == 0:
-            rrc[ k ] = ( np.pi + 4.0 * r - np.pi * r ) / ( np.pi * t_symbol)
+            ir[ k ] = ( np.pi + 4.0 * r - np.pi * r ) / ( np.pi * T_symbol)
 
-        elif r != 0 and np.abs(t_steps[k] ) == t_symbol / ( 4.0 * r ):
-            rrc[ k ] = r * ( -2.0 * np.cos( np.pi * ( 1.0 + r ) / ( 4.0 * r ) )                             + np.pi * np.sin( np.pi * ( 1.0 + r ) / ( 4.0 * r ) ) )                         / ( np.pi * t_symbol )
+        elif r != 0 and np.abs(t_steps[k] ) == T_symbol / ( 4.0 * r ):
+            ir[ k ] = r * ( -2.0 * np.cos( np.pi * ( 1.0 + r ) / ( 4.0 * r ) )                             + np.pi * np.sin( np.pi * ( 1.0 + r ) / ( 4.0 * r ) ) )             / ( np.pi * T_symbol )
 
         else:
-            rrc[ k ] = ( 4.0 * r * t_steps[k] / t_symbol * np.cos( np.pi * ( 1.0 + r ) * t_steps[k] / t_symbol)                         + np.sin( np.pi * (1.0 - r ) * t_steps[k] / t_symbol )) /                         (( 1.0 - ( 4.0 * r * t_steps[k] / t_symbol)**2 ) * np.pi * t_steps[k])
+            ir[ k ] = ( 4.0 * r * t_steps[k] / T_symbol                         * np.cos( np.pi * ( 1.0 + r ) * t_steps[k] / T_symbol)                         + np.sin( np.pi * (1.0 - r ) * t_steps[k] / T_symbol ))             / (( 1.0 - ( 4.0 * r * t_steps[k] / T_symbol)**2 ) * np.pi * t_steps[k])
 
-    return rrc
+    return t_index, ir
 
 
-def get_gaussian_ir(K, n_up, t_symbol, r):
+def get_gaussian_ir(K, r, f_symbol, n_up):
+    """
+    Determines coefficients of an Gaussian filter
+        
+    NOTE: Length of the IR has to be an odd number
+    
+    IN: length of IR (K), roll-off factor (r), symbol rate (f_symbol), upsampling factor (n_up) 
+    OUT: time values (t_index), filter coefficients (ir)
+    """
     
     # check that IR length is odd
     assert K % 2 == 1, 'Length of the impulse response should be an odd number'
     
     # initialize sample time
-    t_sample = t_symbol / n_up
+    T_symbol = 1.0 / f_symbol
+    t_sample = T_symbol / n_up
     
     # time indices and sampled time
     k_steps = np.arange( -(K-1) / 2.0, (K-1) / 2.0 + 1 )   
     t_steps = k_steps * t_sample
+    t_index = np.arange( K ) / f_symbol / n_up
     
-    gaussian = ( r / np.sqrt( np.pi )) * np.exp( -np.square( r * t_steps ))
+    ir = ( r / np.sqrt( np.pi )) * np.exp( -np.square( r * t_steps ))
     
-    return gaussian
+    return t_index, ir
 
 
 # Parameters
@@ -99,7 +121,7 @@ def get_gaussian_ir(K, n_up, t_symbol, r):
 M = 2
 constellation_points = [ -1, 1 ]
 
-t_symbol = 1.0  # symbol time
+f_symbol = 1.0  # sample rate (Baud)
 n_symbol = 1000  # number of symbols
 
 n_up = 100 # samples per symbol (>1 => oversampling)
@@ -112,12 +134,12 @@ r_rc = .33
 r_rrc = .33
 r_gaussian = 0.8
 
-syms_per_filt = 4  # symbols per filter (plus minus in both directions)
+syms_per_filt = 10  # symbols per filter (plus minus in both directions)
 K_filt = 2 * syms_per_filt * n_up + 1         # length of the fir filter
 
-rc = get_rc_ir( K_filt, n_up, t_symbol, r_rc )
-rrc = get_rrc_ir( K_filt, n_up, t_symbol, r_rrc )
-gaussian = get_gaussian_ir( K_filt, n_up, t_symbol, r_gaussian )
+t_rc, rc = get_rc_ir( K_filt, r_rc, f_symbol, n_up )
+t_rrc, rrc = get_rrc_ir( K_filt, r_rrc, f_symbol, n_up )
+t_gaussian, gaussian = get_gaussian_ir( K_filt, r_gaussian, f_symbol, n_up )
 
 import matplotlib.pyplot as plt
 import matplotlib
@@ -131,12 +153,15 @@ plt.rc('text', usetex=True)
 
 matplotlib.rc('figure', figsize=(18, 10) )
 
-plt.plot( np.arange( np.size( rc ) ) * t_symbol / n_up, rc, linewidth=2.0, label='RC' )
-plt.plot( np.arange( np.size( rrc ) ) * t_symbol / n_up, rrc, linewidth=2.0, label='RRC' )
-plt.plot( np.arange( np.size( gaussian ) ) * t_symbol / n_up, gaussian, linewidth=2.0, label='Gaussian' )
+plt.plot( t_rc, rc, linewidth=2.0, label='RC' )
+plt.plot( t_rrc, rrc, linewidth=2.0, label='RRC' )
+plt.plot( t_gaussian, gaussian, linewidth=2.0, label='Gaussian' )
 
 plt.ylim( (-.3, 1.3 ) ) 
 plt.grid( True )
 plt.legend( loc='upper right' )
 plt.title( 'Impulse Responses' )
+
+
+
 
