3950186bab41cc75290873bc649312edecc11568
Dominik Rimpf <dominik.rimpf@student.kit.edu>
2019-11-25 17:41:09 +0100

    [ADD] Added input signal to subplots

--- jupyter-notebooks/eval/signal_propagation.py
+++ jupyter-notebooks/eval/signal_propagation.py
@@ -41,7 +41,7 @@ send_bits = np.random.choice([symbol for symbol in modulation.keys()], size=n_sy
 send_rc = generate_signal(modulation, t_sample_rc, 1/f_symbol, send_bits, rc, syms_per_filt, power)
 
 # add zeros before and after signal (use samples per symbol as factor)
-send_new = zeroing(send_rc, 5)
+send_new = zeroing(send_rc, 5 * int(1/f_symbol/t_sample_rc))
 
 
 ## Transmission
@@ -57,20 +57,31 @@ gamma = 1.3 # [1/W/km]
 output = splitstepfourier(send_new, t_sample_rc, dz, nz, alpha, beta2, gamma, True)
 
 
-fig1, ax1 = plt.subplots(len(output), figsize=(15,30), sharex=True)
+fig1, ax1 = plt.subplots(len(output)+1, figsize=(15,30), sharex=True)
 
 colors = cycle(list(mcolors.TABLEAU_COLORS))
-counter = 0
+
 x_vals = np.arange(send_new.size)*t_sample_rc
 xmin = np.amin(x_vals)
 xmax = np.amax(x_vals)
+counter = 0
+
+ax1[counter].plot(x_vals, np.square(np.abs(send_new)), label='0', color=next(colors))
+ax1[counter].set_xlim(xmin, xmax)
+ax1[counter].set_ylim(bottom=0)
+ax1[counter].set_title("Input (0 km)")
+ax1[counter].set_ylabel("$|s|^2$")
+counter += 1
+
 for key, val in output.items():
-    ax1[counter].plot(x_vals, np.square(abs(val)), label=key, color=next(colors))
-    ax1[counter].set_xlim(xmin, xmax)
+    ax1[counter].plot(x_vals, np.square(np.abs(val)), label=key, color=next(colors))
     ax1[counter].set_ylim(bottom=0)
-    ax1[counter].set_title(f"after {key} steps")
+    ax1[counter].set_title(f"after {key} steps ({float(key)*dz} km)")
     ax1[counter].set_ylabel("$|s|^2$")
     counter += 1
+
+ax1[counter-1].set_title(f"Output ({z_length} km)")
+ax1[counter-1].set_xlabel("t [s]")
 
 
 ## Transmission
@@ -86,7 +97,7 @@ gamma = 1.3 # [1/W/km]
 output2 = splitstepfourier(send_new, t_sample_rc, dz, nz, alpha, beta2, gamma, True)
 
 
-fig2, ax2 = plt.subplots(len(output), figsize=(15,30), sharex=True)
+fig2, ax2 = plt.subplots(len(output)+1, figsize=(15,30), sharex=True)
 
 colors = cycle(list(mcolors.TABLEAU_COLORS))
 counter = 0
@@ -98,17 +109,29 @@ ymax = np.amax(all_vals)*1.1
 x_vals = np.arange(send_new.size)*t_sample_rc
 xmin = np.amin(x_vals)
 xmax = np.amax(x_vals)
+
+ax2[counter].plot(x_vals, np.square(np.abs(send_new)), label='0', color=next(colors))
+ax2[counter].set_xlim(xmin, xmax)
+ax2[counter].set_ylim(bottom=0)
+ax2[counter].set_title(f"Input")
+ax2[counter].set_ylabel("$|s|^2$")
+counter += 1
+
 for key, val in output2.items():
-    ax2[counter].plot(x_vals, np.square(abs(val)), label=key, color=next(colors))
+    ax2[counter].plot(x_vals, np.square(np.abs(val)), label=key, color=next(colors))
     ax2[counter].set_xlim(xmin, xmax)
     ax2[counter].set_ylim(ymin, ymax)
-    ax2[counter].set_title(f"after {key} steps")
+    ax2[counter].set_title(f"after {key} steps ({float(key)*dz} km)")
     ax2[counter].set_ylabel("$|s|^2$")
     counter += 1
     
+ax2[counter-1].set_title(f"Output ({z_length} km)")
 ax2[counter-1].set_xlabel("t [s]")
 
 
 tikzplotlib.save('../../../bachelorarbeit-ausarbeitung/figures/plots/fiber_propagation.tex', figure=fig1)
 tikzplotlib.save('../../../bachelorarbeit-ausarbeitung/figures/plots/fiber_propagation_noalpha.tex', figure=fig2)
+
+
+
 
